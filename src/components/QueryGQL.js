cadcbf829dae9a17f1e9d739b29e0ae135bcdb07
Michael Owuor <michael.owuor@gmail.com>
2022-03-28 00:37:29 +0300

    feat: query component.

---
+++ src/components/QueryGQL.js
@@ -0,0 +1,12 @@
+import { useQuery } from '@apollo/client';
+
+const Query = ({ children, query, slug }) => {
+	const { data, loading, error } = useQuery(query, { variables: { slug: slug } });
+
+	if (loading) return <p>loading...</p>;
+	if (error) return <p>Error: (JSON.stringify(error))</p>;
+
+	return children({ data });
+};
+
+export default Query;
