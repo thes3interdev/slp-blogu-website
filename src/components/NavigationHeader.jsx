a0212d03b14ed46d958bae9ef1bf91b5ef5d53c7
Michael Owuor <michael.owuor@gmail.com>
2022-03-31 17:01:51 +0300

    styles: removed react-icons.

--- src/components/NavigationHeader.jsx
+++ src/components/NavigationHeader.jsx
@@ -12,13 +12,11 @@ const NavigationHeader = () => {
 	const handleClose = () => setIsMenuOpen(!isMenuOpen);
 
 	return (
-		<nav className="fixed inset-x-0 top-0 bg-sky-800">
+		<nav className="fixed inset-x-0 top-0 z-50 bg-sky-800">
 			<div className="mx-auto max-w-7xl px-3 py-5">
 				<div className="relative flex items-center justify-between">
 					<Link to="/" className="inline-flex items-center">
-						<span className="ml-2 text-xl font-semibold uppercase tracking-wide text-slate-100">
-							Blogu
-						</span>
+						<span className="ml-2 text-xl font-semibold uppercase tracking-wide text-slate-100">Blogu</span>
 					</Link>
 
 					<ul className="hidden items-center space-x-8 text-slate-100 lg:flex">
@@ -37,18 +35,9 @@ const NavigationHeader = () => {
 							onClick={() => setIsMenuOpen(true)}
 						>
 							<svg className="w-5 text-slate-100" viewBox="0 0 24 24">
-								<path
-									fill="currentColor"
-									d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-								/>
-								<path
-									fill="currentColor"
-									d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-								/>
-								<path
-									fill="currentColor"
-									d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-								/>
+								<path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z" />
+								<path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z" />
+								<path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z" />
 							</svg>
 						</button>
 
@@ -57,14 +46,8 @@ const NavigationHeader = () => {
 								<div className="rounded border bg-slate-100 p-5 shadow-sm">
 									<div className="mb-4 flex items-center justify-between">
 										<div>
-											<Link
-												onClick={handleClose}
-												to="/"
-												className="inline-flex items-center"
-											>
-												<span className="text-xl font-bold uppercase tracking-wide text-gray-800">
-													Blogu
-												</span>
+											<Link onClick={handleClose} to="/" className="inline-flex items-center">
+												<span className="text-xl font-bold uppercase tracking-wide text-gray-800">Blogu</span>
 											</Link>
 										</div>
 
@@ -84,11 +67,7 @@ const NavigationHeader = () => {
 										<ul className="space-y-4">
 											{navigationLinks.map((link) => (
 												<li key={link.name}>
-													<Link
-														onClick={handleClose}
-														to={link.link}
-														className="text-lg font-semibold tracking-wider"
-													>
+													<Link onClick={handleClose} to={link.link} className="text-lg font-semibold tracking-wider">
 														{link.name}
 													</Link>
 												</li>
