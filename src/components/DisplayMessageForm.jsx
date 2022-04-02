2212ccfa91a5e13f025240304404a27e3149d99b
Michael Owuor <michael.owuor@gmail.com>
2022-04-02 11:08:37 +0300

    feat: update backend data with graphql.

--- src/components/DisplayMessageForm.jsx
+++ src/components/DisplayMessageForm.jsx
@@ -1,49 +1,22 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import CREATE_MESSAGE from '../graphql/mutations/CreateContactMutation';
 
 const DisplayMessageForm = () => {
-	const initialValues = { name: '', email: '', nationality: '', message: '' };
-	const [formValues, setFormValues] = useState(initialValues);
-	const [formErrors, setFormErrors] = useState({});
-	const [isSubmitted, setIsSubmitted] = useState(false);
+	const navigate = useNavigate();
+	const [createContact] = useMutation(CREATE_MESSAGE);
+	const [message, setMessage] = useState({});
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormValues({ ...formValues, [name]: value });
+	const handleChange = (event) => {
+		setMessage({ ...message, [event.target.name]: event.target.value });
 	};
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setFormErrors(validate(formValues));
-		console.log(formValues);
-		setIsSubmitted(true);
-	};
-
-	const validate = (values) => {
-		const errors = {};
-		const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-
-		if (!values.name) {
-			errors.name = 'The name is required.';
-		}
-
-		if (!values.email) {
-			errors.email = 'The email address is required.';
-		} else if (!emailRegex.test(values.email)) {
-			errors.email = 'The email address is not valid.';
-		}
-
-		if (!values.nationality) {
-			errors.nationality = 'The nationality is required.';
-		}
-
-		if (!values.message) {
-			errors.message = 'The message cannot be blank.';
-		} else if (values.message.length < 5) {
-			errors.message = 'Your message is too short and may not make sense.';
-		}
-
-		return errors;
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		createContact({ variables: { ...message } });
+		window.location.reload();
+		navigate(-1);
 	};
 
 	return (
@@ -76,40 +49,37 @@ const DisplayMessageForm = () => {
 									name="name"
 									className="mt-1 w-full rounded-md border-2 border-gray-100 bg-gray-100 py-3 focus:border-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-2"
 									placeholder="John Doe"
-									value={formValues.name}
 									onChange={handleChange}
+									required
 								/>
-								<p className="text-red-500">{formErrors.name}</p>
 							</div>
 							<div className="mb-4 flex-1">
 								<label>
 									Email<span className="font-bold text-red-600">*</span>
 								</label>
 								<input
-									type="text"
+									type="email"
 									name="email"
 									className="mt-1 w-full rounded-md border-2 border-gray-100 bg-gray-100 py-3 focus:border-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-2"
 									placeholder="john.doe@email.com"
-									value={formValues.email}
 									onChange={handleChange}
+									required
 								/>
-								<p className="text-red-500">{formErrors.email}</p>
 							</div>
 						</div>
 						<div className="flex flex-col sm:flex-row sm:gap-8">
 							<div className="mb-5 flex-1">
 								<label>
-									Nationality<span className="font-bold text-red-600">*</span>
+									Country of Residence<span className="font-bold text-red-600">*</span>
 								</label>
 								<input
 									type="text"
 									name="nationality"
 									className="mt-1 w-full rounded-md border-2 border-gray-100 bg-gray-100 py-3 focus:border-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-2"
 									placeholder="Kenya"
-									value={formValues.nationality}
 									onChange={handleChange}
+									required
 								/>
-								<p className="text-red-500">{formErrors.nationality}</p>
 							</div>
 						</div>
 						<div className="flex flex-col sm:flex-row sm:gap-8">
@@ -121,10 +91,9 @@ const DisplayMessageForm = () => {
 									cols="30"
 									rows="5"
 									placeholder="Tell us what we can help you with!"
-									value={formValues.message}
 									onChange={handleChange}
+									required
 								></textarea>
-								<p className="text-red-500">{formErrors.message}</p>
 							</div>
 						</div>
 						<button type="submit" className="action-button">
